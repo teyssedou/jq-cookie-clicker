988d7166e8f8f8133490e1992460b5f60e638bef
teyssedou <mikaelteyssedou@gmail.com>
2017-12-08 11:46:42 +0100

    exos fait

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
-	//votre code
+	
+var clique = 0;
+$(".cake-is-a-lie").click(function() {
+	clique += 1;
+	console.log(clique);
+$(".counter").html(clique);
 
+var text = ["Sors de là!!",
+"Bouge de là!",
+"Mais tu vas sortir OUI!!",
+"Tu sais dehors, il y a de belles choses aussi!",
+"Ferme le Pc et profite de la vie!"];
 
+if (clique%20 == 0) {
+alert (text [Math.floor(Math.random() *5)]);
+}
+});
 });
